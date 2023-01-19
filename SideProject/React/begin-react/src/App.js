497c5474e59f56d8279a5417d04360bbbcbb9a6f
Minsoo <sumin1667@gmail.com>
2023-01-19 17:47:30 +0900

    add side project

---
+++ SideProject/React/begin-react/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Hello from './Hello';
+import './App.css';
+
+function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'black',
+    color: 'aqua',
+    ontSize: 24,
+    padding: '1rem'
+  }
+
+  return (
+    <>
+      <Hello />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
+  )
+}
+
+export default App;
